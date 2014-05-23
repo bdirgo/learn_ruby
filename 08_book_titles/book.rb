0fef1055076b6495cc6e3ceb0110da80b6af144d
Benjamin Dirgo <bdirgo@gmail.com>
2014-05-23 14:09:01 -0700

    finished for now

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,13 +1,15 @@
 class Book
-	def title=(s)
-		words = s.split.map do |word|
-			if %w(the and over).include?(word)
+	attr_accessor :title
+	def title
+		words = @title.split.map do |word|
+			if %w(the in of a an and over).include?(word)
 				word
 			else
 				word.capitalize
 			end
 		end
-		words.first.capitalize!
+		words[0] = words.first.capitalize
+		@title.capitalize
 		words.join(" ")
 
 	end
